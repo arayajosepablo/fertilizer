3129aa9ff63cac9ad3842c9ad6a46dbfed10dd35
Jose Pablo <araya.josepablo@gmail.com>
2022-04-11 11:31:33 -0600

    Formatting files

--- src/main/java/com/flowerbed/fertilizer/FertilizerApplication.java
+++ src/main/java/com/flowerbed/fertilizer/FertilizerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FertilizerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FertilizerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FertilizerApplication.class, args);
+  }
 
 }
--- src/test/java/com/flowerbed/fertilizer/FertilizerApplicationTests.java
+++ src/test/java/com/flowerbed/fertilizer/FertilizerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FertilizerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
